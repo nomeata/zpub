78fb68d39e18a0e2d55666560e1d7020dc394b30
Thomas Breitner <mail@thomas-breitner.de>
2009-01-25 19:15:37 +0000

    linkicons via js

--- templates/static/js/jquery.linkicons.js
+++ templates/static/js/jquery.linkicons.js
@@ -1,15 +1,9 @@
 jQuery(function($) {
 
-
+$(".filelist a[href$='_html']").addClass("htmldir");
 $(".filelist a[href$='.pdf']").addClass("pdf");
 $(".filelist a[href$='.chm']").addClass("chm");
 $(".filelist a[href$='.zip']").addClass("zip");
-
-$(".filelist a[href^='mailto:']").addClass("email");
-
-$('.filelist a').filter(function() {
-    return this.hostname && this.hostname !== location.hostname;
-  }).addClass("external");
 
 /*
 $("#content a[href$='.doc'], a[href$='.txt'], a[href$='.rft']").addClass("txt");

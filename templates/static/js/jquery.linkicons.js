ddeb519df6c0f7e6f79ab98b72b68d8f48ce59d6
Thomas Breitner <mail@thomas-breitner.de>
2009-01-25 19:22:22 +0000

    linkicons via js

--- templates/static/js/jquery.linkicons.js
+++ templates/static/js/jquery.linkicons.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
 
-$(".filelist a[href$='_html']").addClass("htmldir");
+$(".filelist a[href$='_html']").parent.addClass("htmldir");
 $(".filelist a[href$='.pdf']").addClass("pdf");
 $(".filelist a[href$='.chm']").addClass("chm");
 $(".filelist a[href$='.zip']").addClass("zip");

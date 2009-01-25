221b1212cad9d241494695141335d87485b342fb
Thomas Breitner <mail@thomas-breitner.de>
2009-01-25 19:25:44 +0000

    linkicons via js

--- templates/static/js/jquery.linkicons.js
+++ templates/static/js/jquery.linkicons.js
@@ -1,9 +1,9 @@
 jQuery(function($) {
 
 $(".filelist a[href$='_html']").parent("li").addClass("htmldir");
-$(".filelist a[href$='.pdf']").addClass("pdf");
-$(".filelist a[href$='.chm']").addClass("chm");
-$(".filelist a[href$='.zip']").addClass("zip");
+$(".filelist a[href$='.pdf']").parent("li").addClass("pdf");
+$(".filelist a[href$='.chm']").parent("li").addClass("chm");
+$(".filelist a[href$='.zip']").parent("li").addClass("zip");
 
 /*
 $("#content a[href$='.doc'], a[href$='.txt'], a[href$='.rft']").addClass("txt");

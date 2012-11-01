5d1de26ef10ded1c0e64a7dcf9e2b0aab878f27a
Joachim Breitner <mail@joachim-breitner.de>
2012-11-01 19:31:14 +0100

    When creating a new instance, ensure correct persissions of cache/

--- bin/zpub-create-instance.sh
+++ bin/zpub-create-instance.sh
@@ -187,12 +187,18 @@ tell svnadmin create "$ZPUB_INSTANCES/$CUST/repos/source"
 mkdir -v "$ZPUB_INSTANCES/$CUST/repos/source/dav"
 ln -svf "$ZPUB_BIN/zpub-post-commit-hook.sh" "$ZPUB_INSTANCES/$CUST/repos/source/hooks/post-commit"
 
-echo "Making settings and output directories and svn repository owned by and"
+echo "Making cache, settings and output directories and svn repository owned by and"
 echo "writable for group $ZPUB_GROUP."
-chgrp -c -R "$ZPUB_GROUP" "$ZPUB_INSTANCES/$CUST/settings" "$ZPUB_INSTANCES/$CUST/output" \
+chgrp -c -R "$ZPUB_GROUP" \
+	    "$ZPUB_INSTANCES/$CUST/cache" \
+	    "$ZPUB_INSTANCES/$CUST/settings" \
+	    "$ZPUB_INSTANCES/$CUST/output" \
             "$ZPUB_INSTANCES/$CUST/repos/source/"{db,dav}
-chmod -c -R g+w "$ZPUB_INSTANCES/$CUST/settings" "$ZPUB_INSTANCES/$CUST/output" \
-                "$ZPUB_INSTANCES/$CUST/repos/source/"{db,dav}
+chmod -c -R g+w \
+	    "$ZPUB_INSTANCES/$CUST/cache" \
+	    "$ZPUB_INSTANCES/$CUST/settings" \
+	    "$ZPUB_INSTANCES/$CUST/output" \
+            "$ZPUB_INSTANCES/$CUST/repos/source/"{db,dav}
 
 echo
 echo \
